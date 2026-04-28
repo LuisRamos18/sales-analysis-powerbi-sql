d6e6ab29af79c9194b8069b8521eab3aa7a886ff
Luis Ramos <122129431+LuisRamos18@users.noreply.github.com>
2026-04-28 16:22:31 -0600

    Update Dim_Productos.sql

--- sql/Dim_Productos.sql
+++ sql/Dim_Productos.sql
@@ -1,13 +1,18 @@
-/*TABLA DE DIMENSION CLIENTES*/
+/*TABLA DE DIMENSION PRODCUTOS*/
 
 /*---Sistema contpaqi comercial premium V 11.5.1----
 Creado por Luis Ramos
 Fecha de creación 16/04/2026*/
 
-SELECT 
-    CIDCLIENTEPROVEEDOR AS IDCLIENTE, 
-    CCODIGOCLIENTE, 
-    CRAZONSOCIAL,
-    CASE WHEN CIDMONEDA = 1 THEN 'NACIONAL' ELSE 'EXPORTACION' END AS TipoCliente
-FROM admClientes
-WHERE LEFT (CCODIGOCLIENTE, 1) <> 'P'
+SELECT Pro.CIDPRODUCTO,Pro.CCODIGOPRODUCTO AS CodigoOriginal,
+-- Lógica de limpieza para el Código del Producto (quita lo que esté después del '-')
+LEFT(Pro.CCODIGOPRODUCTO, 
+CASE WHEN CHARINDEX('-', Pro.CCODIGOPRODUCTO) > 0 THEN CHARINDEX('-', Pro.CCODIGOPRODUCTO) - 1 
+ELSE LEN(Pro.CCODIGOPRODUCTO) END) AS Producto_Codigo,
+    -- Lógica de limpieza para el Nombre (quita lo que esté después de la '/')
+LEFT(Pro.CNOMBREPRODUCTO, 
+CASE WHEN CHARINDEX('/', Pro.CNOMBREPRODUCTO) > 0 THEN CHARINDEX('/', Pro.CNOMBREPRODUCTO) - 1 
+ELSE LEN(Pro.CNOMBREPRODUCTO) END) AS Producto_Nombre,
+Uni.CIDUNIDAD,Uni.CNOMBREUNIDAD AS UnidadBase,Uni.CABREVIATURA AS UnidadAbreviada
+FROM [dbo].[admProductos] AS Pro
+INNER JOIN [dbo].[admUnidadesMedidaPeso] AS Uni ON Uni.CIDUNIDAD = Pro.CIDUNIDADBASE
